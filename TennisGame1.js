dddedaf7dd2894727be9b63a6c4b3c3ba1224dba
Néstor Salceda <nestor.salceda@gmail.com>
2018-04-02 13:27:42 +0200

    Extract a player class

--- TennisGame1.js
+++ TennisGame1.js
@@ -5,48 +5,71 @@ const scores = {
   3: "Forty"
 }
 
-const winFor = player => `Win for ${player}`;
-const advantageFor = player => `Advantage ${player}`;
+const winFor = player => `Win for ${player.name}`;
+const advantageFor = player => `Advantage ${player.name}`;
+const printPoints = (player1, player2) => `${scores[player1.points]}-${scores[player2.points]}`
+
+class Player {
+  constructor(name) {
+    this.name = name
+    this.points = 0
+  }
+
+  addPoint() {
+    this.points++
+  }
+
+  get isFinalPoint() {
+    return this.points >= 4
+  }
+
+  isTied(player) {
+    return this.points == player.points
+  }
+}
+
+const DIFFERENCE_POINTS_THRESHOLD = 2
 
 class TennisGame1 {
+
   constructor (player1Name, player2Name) {
-    this.player1Points = 0;
-    this.player2Points = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+    this.player1 = new Player(player1Name)
+    this.player2 = new Player(player2Name)
   };
 
   wonPoint (playerName) {
-    playerName === this.player1Name ? this.player1Points++ : this.player2Points++;
+    this._playerWithName(playerName).addPoint();
   };
 
-  getScore () {
-    if (this._isTied) return this._scoreForTied();
-    if (this._isGameEnded) return winFor(this._winnerPlayer)
-    if (this._isFinalPoint) return advantageFor(this._winnerPlayer)
-
-    return `${scores[this.player1Points]}-${scores[this.player2Points]}`
-  };
-
-  get _isTied() {
-    return this.player1Points === this.player2Points;
+  _playerWithName(playerName) {
+    return playerName === this.player1.name ? this.player1 : this.player2;
   }
 
+  get _winningPlayer() {
+    return this.player1.points > this.player2.points ? this.player1: this.player2;
+  }
+
+  getScore () {
+    if (this.player1.isTied(this.player2)) return this._scoreForTied();
+    if (this._isGameEnded) return winFor(this._winningPlayer)
+    if (this._isFinalPoint) return advantageFor(this._winningPlayer)
+
+    return printPoints(this.player1, this.player2)
+  };
+
   get _isFinalPoint() {
-    return this.player1Points >= 4 || this.player2Points >= 4
+    return this.player1.isFinalPoint || this.player2.isFinalPoint
   }
 
   _scoreForTied() {
-    return (this.player1Points < 3) ? scores[this.player1Points] + '-All' : "Deuce"
+    return (this.player1.points < 3) ? scores[this.player1.points] + '-All' : "Deuce"
   }
 
   get _isGameEnded() {
-    return this._isFinalPoint && Math.abs(this.player1Points - this.player2Points) >= 2;
+    return this._isFinalPoint && Math.abs(this.player1.points - this.player2.points) >= DIFFERENCE_POINTS_THRESHOLD;
   }
 
-  get _winnerPlayer() {
-    return this.player1Points > this.player2Points ? this.player1Name : this.player2Name;
-  }
+
 };
 
 if (typeof window === "undefined") {
